742e7438678ad457c551c524476c3bbd4a1e82d5
Mike Verderese <verderese@renew.com>
2018-01-23 16:29:42 -0800

    Added additional argument for specifying key name:
    `ec2ssh development-mike 2 renew_development.pem`

--- ssh/ec2ssh.sh
+++ ssh/ec2ssh.sh
@@ -6,6 +6,7 @@ if [[ -z "$1" ]]; then
 fi
 
 env=$1
+key_name=$3
 filter="Name=tag:environment,Values=$env"
 query="Reservations[].Instances[?PublicDnsName!=\`\`].[Tags[?Key==\`class\`].Value|[0], PublicDnsName, InstanceId][]"
 
@@ -13,7 +14,7 @@ cmd="aws ec2 describe-instances --filters '$filter' --query '$query' --output te
 
 if [[ -n "$2" ]]; then
     hostname=$(eval "$cmd" | head -"$2" | tail -1 | awk '{print $3}')
-    exec ssh -A -i ~/.ssh/renew_${env}.pem ubuntu@${hostname}
+    exec ssh -A -i ~/.ssh/${key_name:-renew_${env}.pem} ubuntu@${hostname}
 else
     eval "$cmd"
 fi
