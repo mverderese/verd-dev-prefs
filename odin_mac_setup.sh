f03f8926ebf1a7567c85db676e72e0b7f3e0fc6f
Mike Verderese <mike@useodin.com>
2021-08-22 09:33:50 -0400

    More cleanup

--- odin_mac_setup.sh
+++ odin_mac_setup.sh
@@ -10,6 +10,25 @@ echo "Done!\n"
 echo "Installing useful tools..."
 brew install tree
 brew install pgcli
+brew install golang-migrate
+echo '
+
+# Docker convenience functions
+docker-stop-all() {
+    docker stop $(docker ps -aq)
+}
+
+docker-destroy-all() {
+     # Stop all containers
+     docker stop $(docker ps -a -q)
+     # Delete all containers
+     docker rm $(docker ps -a -q)
+     # Delete all images
+     docker rmi $(docker images -q) -f
+     # Delete all networks
+     docker network rm $(docker network ls -q)
+}
+' >> ~/.zshrc
 echo "Done!\n"
 
 echo "Installing Python dependencies..."
@@ -34,12 +53,22 @@ echo "Installing Python 3.9.6..."
 pyenv install 3.9.6
 pyenv global 3.9.6
 pyenv exec pip install --upgrade pip
+echo '
+
+# Ensure shell path uses pyenv shims
+eval "$(pyenv init --path)"
+' >> ~/.zprofile
 echo "Done!\n"
 
 echo "Installing Node 14.17.5..."
 brew install nodenv
 nodenv install 14.17.5
 nodenv global 14.17.5
+echo '
+
+# Ensure shell path uses nodenv shims
+eval "$(nodenv init -)"
+' >> ~/.zprofile
 echo "Done!\n"
 
 echo "Installing Docker..."
@@ -49,6 +78,13 @@ echo "Done!\n"
 
 echo "Installing Google Cloud SDK..."
 brew install --cask google-cloud-sdk
+echo '
+
+# Set shell path to see gcloud and enabled shell completion
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+' >> ~/.zshrc
+
 echo "Done!\n"
 
 echo "Setup of development tools complete... Bye!"
